e8f2745b0e34a658bbc6f8d09b26648165c01a0b
Sean <sean@vestaridefair.com>
2021-06-22 17:00:00 -0700

    wooooooohoo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import React, { Component } from 'react';
-import Notes from './components/pages/Notes'
-import Create from './components/pages/Create'
-import SeanFirstForm from './components/old/SeanFirstForm';
+import Notes from './pages/Notes'
+import Create from './pages/Create'
 import './App.css';
-import FormikContainer from './components/old/FormikContainer';
-import FormDemo from './components/FormDemo';
-import LetsCodeDemo from './components/LetsCodeDemo';
+
 
 
 function App() {
---
+++ src/pages/Create.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+
+export default function Create() {
+  return (
+    <div>
+      Create page
+      <Typography variant="h1">
+        Create a new note
+      </Typography>
+
+    </div>
+  )
+}
