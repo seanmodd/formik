b0fd0a666877d302039771be1c06109c7b9c8047
Sean <sean@vestaridefair.com>
2021-06-23 14:16:12 -0700

    theme editor

--- src/App.js
+++ src/App.js
@@ -3,21 +3,39 @@ import React, { Component } from 'react';
 import Notes from './pages/Notes'
 import Create from './pages/Create'
 import './App.css';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core"
+import { purple } from '@material-ui/core/colors';
 
-
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fefe"
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Poppins',
+    fontWeightLight: 100,
+    fontWeightRegular: 400,
+    fontWeightMedium: 400,
+    fontWeightBold: 700
+  }
+})
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,29 +1,68 @@
 import React from 'react'
 import { Button, ButtonGroup, Box, Container, Paper } from '@material-ui/core'
 // import { MenuIcon } from '@material-ui/icons'
-import { makeStyles } from '@material-ui/core/styles';
+// import { AcUnitOutlinedIcon } from '@material-ui/icons'
+// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import "@fontsource/roboto"; // Defaults to weight 400.
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
+import SendIcon from '@material-ui/icons/Send';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+// import "@fontsource/roboto"; // Defaults to weight 400.
+
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: "underline",
+    color: 'red'
+  },
+  spacing: {
+    marginTop: 50,
+    height: "100vh",
+    width: "100vw",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+}
+)
 
 export default function Create() {
+  const classes = useStyles()
   return (
     <div>
 
 
       <AppBar component="div" alignItems="center" justifyContent="center" width={10} elevation={10}>
-        <Typography variant="button" align="center">
+        <Typography className={classes.title} variant="button" align="center">
           Create page
         </Typography>
       </AppBar>
-      <Container>
+      <Container className={classes.spacing}>
         <Typography variant="h1" display="block" color="" align="center" >
           Create a new note.
         </Typography>
-        <Button variant="contained" color="secondary" type="submit" onClick={() => console.log("you clicked me")}>Submit</Button>
+        <Button
+          className={classes.btn}
+          variant="contained"
+          color="secondary"
+          type="submit"
+          onClick={() => console.log("you clicked me")}
+          startIcon={<SendIcon />}
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
 
 
         <ButtonGroup color="secondary" >
@@ -31,6 +70,14 @@ export default function Create() {
           <Button>Two</Button>
           <Button>Three</Button>
         </ButtonGroup>
+
+        {/* icons */}
+        <br />
+        <AcUnitOutlinedIcon />
+        <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+        <AcUnitOutlinedIcon color="action" fontSize="small" />
+        <AcUnitOutlinedIcon color="error" fontSize="small" />
+        <AcUnitOutlinedIcon color="disabled" fontSize="small" />
       </Container>
     </div>
   )
